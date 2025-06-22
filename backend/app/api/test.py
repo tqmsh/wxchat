6fc5c1600c87915141e22ade61643705cb7e9602
kkathereliu <katherineliu327kl@gmail.com>
2025-06-21 20:58:48 -0400

    delete old/unused files

--- backend/app/api/test.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/test")
-def test_route():
-    return {"message": "Hello, world!"}
--- backend/app/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from app.api import conversation, test
-
-app = FastAPI()
-app.include_router(conversation.router)
-app.include_router(test.router)
